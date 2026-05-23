06687d3dba0ca1f7b5614bafdac964a53b7aedc3
Ajinkya Joshi <ajinkya.joshi.d@gmail.com>
2026-05-23 17:08:49 -0400

    Create outputs.tf

---
+++ modules/eks/outputs.tf
@@ -0,0 +1,39 @@
+output "cluster_id" {
+  value = aws_eks_cluster.main.id
+}
+
+output "cluster_arn" {
+  value = aws_eks_cluster.main.arn
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "cluster_version" {
+  value = aws_eks_cluster.main.version
+}
+
+output "cluster_oidc_issuer_url" {
+  value = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+output "cluster_security_group_id" {
+  value = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+}
+
+output "irsa_role_arns" {
+  description = "IRSA role ARNs — consumed as env vars by Helmfile"
+  value = {
+    ebs_csi        = aws_iam_role.ebs_csi.arn
+    alb_controller = aws_iam_role.alb_controller.arn
+    vpc_cni        = aws_iam_role.vpc_cni.arn
+    karpenter      = aws_iam_role.karpenter.arn
+    external_dns   = aws_iam_role.external_dns.arn
+    vault          = aws_iam_role.vault.arn
+  }
+}
