424c88ff78116494189a6015c9e012b7c30cd30d
Ajinkya Joshi <ajinkya.joshi.d@gmail.com>
2026-05-23 17:09:32 -0400

    Create ecr.tf

---
+++ modules/supporting/ecr.tf
@@ -0,0 +1,35 @@
+# ECR repos + lifecycle policies
+
+resource "aws_ecr_repository" "app" {
+  name                 = "${var.eks_cluster_name}/app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Name = "${var.eks_cluster_name}-ecr"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Keep last 30 images"
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 30
+      }
+      action = { type = "expire" }
+    }]
+  })
+}
