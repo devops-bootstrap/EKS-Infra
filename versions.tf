611e9a63c4454bc05a491cad843e9df523529ad3
Ajinkya Joshi <ajinkya.joshi.d@gmail.com>
2026-05-23 17:06:01 -0400

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = var.default_tags
+  }
+}
