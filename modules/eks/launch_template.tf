2911e55380e4c4d3e0a4d482d91abc04918251bc
Ajinkya Joshi <ajinkya.joshi.d@gmail.com>
2026-05-23 17:08:12 -0400

    Create launch_template.tf

---
+++ modules/eks/launch_template.tf
@@ -0,0 +1,28 @@
+# IMDSv2, encrypted EBS, Bottlerocket
+
+resource "aws_launch_template" "nodes" {
+  name_prefix = "${var.eks_cluster_name}-node-"
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required" # IMDSv2 enforced
+    http_put_response_hop_limit = 1
+  }
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size           = 50
+      volume_type           = "gp3"
+      encrypted             = true
+      delete_on_termination = true
+    }
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.eks_cluster_name}-node"
+    }
+  }
+}
