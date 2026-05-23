44c96ab80c3a68446a793fe270b4d078e7a2bb72
Ajinkya Joshi <ajinkya.joshi.d@gmail.com>
2026-05-23 17:08:37 -0400

    Create node_groups.tf

---
+++ modules/eks/node_groups.tf
@@ -0,0 +1,71 @@
+# System (on-demand) + App (spot) node groups
+
+resource "aws_eks_node_group" "system" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.eks_cluster_name}-system"
+  node_role_arn   = aws_iam_role.node_group.arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = var.eks_node_group_desired_size
+    min_size     = var.eks_node_group_min_size
+    max_size     = var.eks_node_group_max_size
+  }
+
+  ami_type       = var.eks_node_group_ami_type
+  capacity_type  = "ON_DEMAND"
+  instance_types = var.eks_node_group_instance_types
+
+  update_config {
+    max_unavailable = 1
+  }
+
+  launch_template {
+    id      = aws_launch_template.nodes.id
+    version = aws_launch_template.nodes.latest_version
+  }
+
+  labels = {
+    Environment = var.environment
+    role        = "system"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.node_worker,
+    aws_iam_role_policy_attachment.node_cni,
+    aws_iam_role_policy_attachment.node_ecr,
+  ]
+
+  tags = {
+    Name = "${var.eks_cluster_name}-system-ng"
+  }
+}
+
+# Node Group IAM Role
+resource "aws_iam_role" "node_group" {
+  name = "${var.eks_cluster_name}-nodegroup-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_worker" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_cni" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "node_ecr" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_group.name
+}
